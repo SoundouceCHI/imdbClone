3de06c61f0e7e013149f514e341c17815fc00a25
SoundouceCHI <soundouce.chibani@gmail.com>
2023-11-05 02:23:28 +0100

    Add the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,20 @@
+export default function About(){
+    return <div className="max-w-6xl mx-auto space-y-4">
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+        <p>
+        Welcome to our movie database website, the comprehensive cinema encyclopedia that cinephiles and casual moviegoers alike call home. Founded in [Year], our database is a vibrant hub where film history and the latest silver screen wonders coalesce, providing a tapestry of cinematic artistry at your fingertips.
+        Our Mission: To ignite passion for film in every visitor by offering an extensive, meticulously curated, and easily navigable repository of film information. From the Golden Age of Hollywood to the avant-garde of world cinema, our database casts a spotlight on them all.
+        </p>
+        <p>What We Offer:
+        Expansive Movie Listings: Delve into a plethora of films across diverse genres, each accompanied by detailed synopses, critical ratings, and behind-the-scenes insights.
+        Dynamic Search Features: Our advanced search functionality allows you to find films by title, genre, director, actor, or even by specific dialogues or quotes.
+        Community Engagement: Share your reviews, rate movies, and join discussions. [Your Site Name] is a platform for vibrant dialogue and shared enthusiasm.
+        Industry News: Stay up-to-date with the latest in film news, box office reports, festival updates, and upcoming releases.
+        Exclusive Interviews & Features: Gain unique perspectives through our exclusive interviews with film professionals, from emerging talents to seasoned veterans.
+        Educational Resources: For the film scholar and the curious learner, we offer articles, essays, and resources that delve deep into film theory and history.
+        </p>
+        <p>
+        Our database is more than just a collection of titles. It's a gateway to the stories that shape our world, the artistry that inspires us, and the community that finds common ground in the shared love of film.
+        </p>
+    </div>; 
+}
