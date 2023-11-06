9380698d2667ca5368dc2581d5c7d53dc4755f89
SoundouceCHI <soundouce.chibani@gmail.com>
2023-11-06 21:17:45 +0100

    Fetch data from tmdb api in the home page

---
+++ src/components/Results.jsx
@@ -0,0 +1,14 @@
+export default function Results({results}){
+    
+    return (
+        <div>
+            {
+                results.map((results)=> (
+                    <div key={results.id}> 
+                        {results.original_title}
+                    </div>
+                ))
+            }
+        </div>
+    )
+}
