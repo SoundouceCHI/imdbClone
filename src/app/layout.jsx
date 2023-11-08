006bfa5d770a0818bcdee6a3ec3c346210a7a8fe
SoundouceCHI <soundouce.chibani@gmail.com>
2023-11-08 13:12:40 +0100

    fix some error

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,4 @@
-import Header from '@/components/header'
+import Header from '@/components/Header'
 import './globals.css'
 import Providers from "./Providers"
 import Navbar from '@/components/Navbar'
